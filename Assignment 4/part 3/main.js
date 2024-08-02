a43fa92dbe934335f5e6bdf4f68e222a9a23335f
KyleI24 <kyle.intendencia@dcmail.ca>
2024-08-02 17:29:09 -0400

    removing the testball after finishing the full project

--- Assignment 4/part 3/main.js
+++ Assignment 4/part 3/main.js
@@ -56,12 +56,6 @@ class Ball {
       }
       
   }
-const testBall = new Ball(50, 100, 4, 4, "blue", 10);
-testBall.x;
-testBall.size;
-testBall.color;
-testBall.draw();
-
 const balls = [];
 
 while (balls.length < 25) {
