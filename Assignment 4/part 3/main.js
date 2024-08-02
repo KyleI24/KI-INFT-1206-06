ed00f3752490d6424b5063cbf311c803b6d71156
KyleI24 <kyle.intendencia@dcmail.ca>
2024-08-02 18:22:01 -0400

    created the shape class

--- Assignment 4/part 3/main.js
+++ Assignment 4/part 3/main.js
@@ -18,14 +18,22 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-    constructor(x, y, velX, velY, color, size) {
+class Shape {
+
+    constructor(x, y, velX, velY) {
       this.x = x;
       this.y = y;
       this.velX = velX;
       this.velY = velY;
+    }
+  
+  }
+
+class Ball extends Shape {
+    constructor(x, y, velX, velY, color, size) {
       this.color = color;
       this.size = size;
+      this.exists = true
     }
     
     draw() {
@@ -54,7 +62,19 @@ class Ball {
         this.x += this.velX;
         this.y += this.velY;
       }
-      
+      collisionDetect() {
+        for (const ball of balls) {
+           if (!(this === ball) && ball.exists) {
+              const dx = this.x - ball.x;
+              const dy = this.y - ball.y;
+              const distance = Math.sqrt(dx * dx + dy * dy);
+   
+              if (distance < this.size + ball.size) {
+                ball.color = this.color = randomRGB();
+              }
+           }
+        }
+     }
   }
 const balls = [];
 
