9b540d3b125f33fa2b9345b7dd2edf0cb9b3ecba
KyleI24 <kyle.intendencia@dcmail.ca>
2024-08-02 16:30:04 -0400

    added images with their alt name

--- Assignment 4/part 2/main.js
+++ Assignment 4/part 2/main.js
@@ -6,7 +6,17 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+
 /* Declaring the alternative text for each image file */
+
+const alts = {
+    'pic1.jpg' : 'a human eye',
+    'pic2.jpg' : 'a rock shaped in a wave',
+    'pic3.jpg' : 'purple and white flowers',
+    'pic4.jpg' : 'drawings of a tomb',
+    'pic5.jpg' : 'an insect with wings'
+  }
 
 /* Looping through images */
 
