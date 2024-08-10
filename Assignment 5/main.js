57e0f308f66cb4351f1bb2cb7870e3c54ec6163b
KyleI24 <kyle.intendencia@dcmail.ca>
2024-08-10 17:40:09 -0400

    finishing the final touches making buttons function and colored and styled to be more pleasing

--- Assignment 5/main.js
+++ Assignment 5/main.js
@@ -15,7 +15,7 @@ showHideBtn.onclick = function() {
     commentWrapper.style.display = 'none';
   }
 };
-
+  
 // functionality for adding a new comment via the comments form
 
 const form = document.querySelector('.comment-form');
@@ -30,18 +30,61 @@ form.onsubmit = function(e) {
 
 function submitComment() {
   const listItem = document.createElement('li');
+  const nameLabel = document.createElement('span');
+  const commentLabel = document.createElement('span');
   const namePara = document.createElement('p');
   const commentPara = document.createElement('p');
   const nameValue = nameField.value;
   const commentValue = commentField.value;
 
-  namePara.textContent = nameValue;
-  commentPara.textContent = commentValue;
+  namePara.setAttribute("tabindex", "0");
+  commentPara.setAttribute("tabindex", "0");
+
+  nameLabel.textContent = "Author: ";
+  commentLabel.textContent = "Comment: ";
+
+  namePara.appendChild(nameLabel);
+  commentPara.appendChild(commentLabel);
+  
+  namePara.innerHTML += nameValue;
+  commentPara.innerHTML += commentValue;
+
+  console.log("attr: ", namePara.getAttribute("tabindex"));
 
   list.appendChild(listItem);
   listItem.appendChild(namePara);
   listItem.appendChild(commentPara);
 
+  console.log("namePara: ", namePara);
+
   nameField.value = '';
   commentField.value = '';
 }
+
+console.log("here")
+const transcript = document.querySelector('.transcript');
+const transcriptBtn = document.querySelector('.transcript-container button');
+
+transcriptBtn.addEventListener("click", toggleTranscript);
+
+function toggleTranscript() {
+  if(transcriptBtn.textContent === 'Show transcript') {
+    transcript.style.height = '150px';
+    transcriptBtn.textContent = 'Hide transcript';
+  } else {
+    transcript.style.height = '0';
+    transcriptBtn.textContent = 'Show transcript';
+  }
+};
+
+const commentBtn = document.querySelector(".show-hide")
+
+commentBtn.addEventListener("keydown", (e) => {
+  if(e.keyCode === 13) {
+    document.activeElement.click();
+  }
+});
+const skipToContentLink = document.querySelector("#skip-to-content-link");
+skipToContentLink.addEventListener("focus", (e) => {
+  skipToContentLink.classList.toggle("skip-to-content-link-visible");
+})
